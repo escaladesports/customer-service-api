d05a5cd789e884a388ed058439a567ce56684483
Christian Sedlacek <csedlacek@escaladesports.com>
2017-10-24 15:04:34 -0500

    Updated email config to have warranty claim recipients field instead of quote recipients

--- config/email.config.js
+++ config/email.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	quoteRequestRecipients: [
+	warrantyClaimRecipients: [
 		{email: 'christian.l.sedlacek@gmail.com', name: 'Christian Sedlacek'},
 		{email: 'csedlacek@escaladesports.com', name: 'Christian Sedlacek'}
 	]
