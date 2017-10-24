05f8a35c97d1f71ece0d945ead7717e5640d1322
Christian Sedlacek <csedlacek@escaladesports.com>
2017-10-24 16:24:51 -0500

    Updated storage config to have correct categories

--- config/store.config.js
+++ config/store.config.js
@@ -1,19 +1,16 @@
 module.exports = {
-	spreadsheetId: '1OdwE48icgXO67g3JiokcuBN8ZV0xtlG89UHenEUQ55g',
+	spreadsheetId: '17bLYtG8zmZjttHJePBODOYkm30tI31YdE5Yvn5Tpl88',
 	spreadsheetNames: {
 		// category-key : spreadsheet-name
 		"air-hockey" : 'air-hockey',
-		"arcade" : 'arcade',
-		"archery" : 'archery',
 		"basketball" : 'basketball',
 		"billiards" : 'billiards',
 		"darting" : 'darting',
-		"fitness" : 'fitness',
 		"foosball" : 'foosball',
 		"multi-games" : 'multi-games',
-		"playsets" : 'playsets',
+		"outdoor-games" : 'outdoor-games',
+		"pickleball" : 'pickleball',
 		"table-tennis" : 'table-tennis',
-		"utility-weights" : 'utility-weights',
 		"other" : 'other'
 	}
 }
