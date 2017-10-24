7c15ceefe9aa534f82e5daf5607545dbf3190edb
Christian Sedlacek <csedlacek@escaladesports.com>
2017-10-24 16:04:57 -0500

    Updated datastore config to tie category keys to spreadsheet names

--- config/store.config.js
+++ config/store.config.js
@@ -1,4 +1,19 @@
 module.exports = {
 	spreadsheetId: '1OdwE48icgXO67g3JiokcuBN8ZV0xtlG89UHenEUQ55g',
-	spreadsheetName: 'quote-requests'
+	spreadsheetNames: {
+		// category-key : spreadsheet-name
+		"air-hockey" : 'air-hockey',
+		"arcade" : 'arcade',
+		"archery" : 'archery',
+		"basketball" : 'basketball',
+		"billiards" : 'billiards',
+		"darting" : 'darting',
+		"fitness" : 'fitness',
+		"foosball" : 'foosball',
+		"multi-games" : 'multi-games',
+		"playsets" : 'playsets',
+		"table-tennis" : 'table-tennis',
+		"utility-weights" : 'utility-weights',
+		"other" : 'other'
+	}
 }
