4eee3e5a2da6b46fc80e0f8812b08ec593587c45
Christian Sedlacek <csedlacek@escaladesports.com>
2017-10-24 15:38:13 -0500

    Updated email config format to have multiple recipient lists

--- config/email.config.js
+++ config/email.config.js
@@ -1,6 +1,45 @@
 module.exports = {
-	warrantyClaimRecipients: [
-		{email: 'christian.l.sedlacek@gmail.com', name: 'Christian Sedlacek'},
-		{email: 'csedlacek@escaladesports.com', name: 'Christian Sedlacek'}
-	]
+	warrantyClaimRecipients: {
+		"air-hockey" : [
+			{email: 'christian.l.sedlacek@gmail.com', name: 'Christian Sedlacek'}
+		],
+		"arcade" : [
+			{email: 'csedlacek@escaladesports.com', name: 'Christian Sedlacek'}
+		],
+		"archery" : [
+			{email: 'christian.l.sedlacek@gmail.com', name: 'Christian Sedlacek'}
+		],
+		"basketball" : [
+			{email: 'csedlacek@escaladesports.com', name: 'Christian Sedlacek'}
+		],
+		"billiards" : [
+			{email: 'christian.l.sedlacek@gmail.com', name: 'Christian Sedlacek'},
+			{email: 'csedlacek@escaladesports.com', name: 'Christian Sedlacek'}
+		],
+		"darting" : [
+			{email: 'christian.l.sedlacek@gmail.com', name: 'Christian Sedlacek'}
+		],
+		"fitness" : [
+			{email: 'christian.l.sedlacek@gmail.com', name: 'Christian Sedlacek'},
+			{email: 'csedlacek@escaladesports.com', name: 'Christian Sedlacek'}
+		],
+		"foosball" : [
+			{email: 'christian.l.sedlacek@gmail.com', name: 'Christian Sedlacek'}
+		],
+		"multi-games" : [
+			{email: 'christian.l.sedlacek@gmail.com', name: 'Christian Sedlacek'}
+		],
+		"playsets" : [
+			{email: 'christian.l.sedlacek@gmail.com', name: 'Christian Sedlacek'}
+		],
+		"table-tennis" : [
+			{email: 'christian.l.sedlacek@gmail.com', name: 'Christian Sedlacek'}
+		],
+		"utility-weights" : [
+			{email: 'christian.l.sedlacek@gmail.com', name: 'Christian Sedlacek'}
+		],
+		"other" : [
+			{email: 'christian.l.sedlacek@gmail.com', name: 'Christian Sedlacek'}
+		]
+	}
 }
