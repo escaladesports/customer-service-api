7a2dec1d2fd953f64b2a647972ae9089af9a2163
Christian Sedlacek <csedlacek@escaladesports.com>
2017-10-25 14:48:30 -0500

    Added contact emails to config

--- config/email.config.js
+++ config/email.config.js
@@ -32,5 +32,46 @@ module.exports = {
 		"other" : [
 			{email: 'christian.l.sedlacek@gmail.com', name: 'Christian Sedlacek'}
 		]
+	},
+	contactRecipients: {
+		"air-hockey" : [
+			{email: 'christian.l.sedlacek@gmail.com', name: 'Christian Sedlacek'}
+		],
+		"arcade" : [
+			{email: 'christian.l.sedlacek@gmail.com', name: 'Christian Sedlacek'}
+		],
+		"archery" : [
+			{email: 'christian.l.sedlacek@gmail.com', name: 'Christian Sedlacek'}
+		],
+		"basketball" : [
+			{email: 'christian.l.sedlacek@gmail.com', name: 'Christian Sedlacek'}
+		],
+		"billiards" : [
+			{email: 'christian.l.sedlacek@gmail.com', name: 'Christian Sedlacek'}
+		],
+		"darting" : [
+			{email: 'christian.l.sedlacek@gmail.com', name: 'Christian Sedlacek'}
+		],
+		"fitness" : [
+			{email: 'christian.l.sedlacek@gmail.com', name: 'Christian Sedlacek'}
+		],
+		"foosball" : [
+			{email: 'christian.l.sedlacek@gmail.com', name: 'Christian Sedlacek'}
+		],
+		"multi-games" : [
+			{email: 'christian.l.sedlacek@gmail.com', name: 'Christian Sedlacek'}
+		],
+		"playsets" : [
+			{email: 'christian.l.sedlacek@gmail.com', name: 'Christian Sedlacek'}
+		],
+		"table-tennis" : [
+			{email: 'christian.l.sedlacek@gmail.com', name: 'Christian Sedlacek'}
+		],
+		"utility-weights" : [
+			{email: 'christian.l.sedlacek@gmail.com', name: 'Christian Sedlacek'}
+		],
+		"other" : [
+			{email: 'christian.l.sedlacek@gmail.com', name: 'Christian Sedlacek'}
+		]
 	}
 }
